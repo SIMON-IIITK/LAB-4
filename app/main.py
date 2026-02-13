ae81b44e16645ab42066ddff1e6fe68ed8006f73
SIMON-IIITK <121494406+SIMON-IIITK@users.noreply.github.com>
2026-02-13 22:59:50 +0530

    Added Jenkins Image

--- app/main.py
+++ app/main.py
@@ -1,20 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 import numpy as np
 
 app = FastAPI()
 
-# Load trained model
 model = joblib.load("model/model.pkl")
+
+class WineInput(BaseModel):
+    features: list[float]
 
 @app.get("/")
 def home():
     return {"message": "Wine Quality Inference API"}
 
 @app.post("/predict")
-def predict(features: list):
-    data = np.array(features).reshape(1, -1)
-    prediction = model.predict(data)[0]
+def predict(data: WineInput):
+    arr = np.array(data.features).reshape(1, -1)
+    prediction = model.predict(arr)[0]
 
     return {
         "name": "Simon",
